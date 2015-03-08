a09c0dfefec3ba2cb12e13f5fa40a6c6baace16f
Brett Wallace <bwallace90@gmail.com>
2015-03-07 19:39:41 -0500

    Add Slot struct

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Brett Wallace on 3/7/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
