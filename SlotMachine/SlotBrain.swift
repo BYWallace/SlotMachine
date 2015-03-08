68f6d8994c83117ce035f5da80f9b19af2749d43
Brett Wallace <bwallace90@gmail.com>
2015-03-08 14:48:54 -0400

    Convert slots into rows by adding SlotBrain class

---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,34 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by Brett Wallace on 3/8/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+class SlotBrain {
+    class func unpackSlotsIntoSlotRows(slots: [[Slot]]) -> [[Slot]] {
+        var slotRow: [Slot] = []
+        var slotRow2: [Slot] = []
+        var slotRow3: [Slot] = []
+        
+        for slotArray in slots {
+            for var index = 0; index < slotArray.count; index++ {
+                let slot = slotArray[index]
+                if index == 0 {
+                    slotRow.append(slot)
+                } else if index == 1 {
+                    slotRow2.append(slot)
+                } else if index == 2 {
+                    slotRow3.append(slot)
+                } else {
+                    println("Error")
+                }
+            }
+        }
+        var slotsInRows: [[Slot]] = [slotRow, slotRow2, slotRow3]
+        return slotsInRows
+    }
+}
